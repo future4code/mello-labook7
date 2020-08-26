6e1c5e8c30cbbaabe74e6f2485ecd9ed175d2b2b
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-08-26 16:45:22 -0300

    Aplicando funcionalidade de pegar todo feed

--- src/data/FriendDataBase.ts
+++ src/data/FriendDataBase.ts
@@ -3,7 +3,10 @@ import { BaseDatabase } from "./BaseDatabase";
 export class FriendDatabase extends BaseDatabase {
   private static TABLE_NAME: string = "Friendship";
 
-  public async makeFriendship(user_id: string, friend_id: string): Promise<void> {
+  public async makeFriendship(
+    user_id: string,
+    friend_id: string
+  ): Promise<void> {
     await this.getConnection()
       .insert({
         user_id,
@@ -11,7 +14,7 @@ export class FriendDatabase extends BaseDatabase {
       })
       .into(FriendDatabase.TABLE_NAME);
   }
-  
+
   public async undoFriendship(
     user_id: string,
     friend_id: string
@@ -23,35 +26,35 @@ export class FriendDatabase extends BaseDatabase {
       .andWhere({ user_id });
   }
 
-  public async getFriendshipById(id: string) :Promise<any> {
+  public async getFriendshipById(id: string): Promise<any[]> {
     const result = await this.getConnection()
       .select("*")
       .from(FriendDatabase.TABLE_NAME)
       .where({
-        user_id: id
+        user_id: id,
       })
       .orWhere({
-        friend_id: id
-      })
+        friend_id: id,
+      });
 
-      return result;
+    return result;
   }
 
-  //   public async getFeed(user_id: string): Promise<any> {
-  //     const response = await this.getConnection().raw(`
-  //     SELECT
-  // 	    r.id,
-  // 	    r.title,
-  // 	    r.description,
-  // 	    r.creation_date AS createdAt,
-  // 	    u.id AS userId,
-  // 	    u.name AS userName
-  //     FROM
-  // 	    Follow_Relation f JOIN Users u ON f.follow_id = u.id
-  // 	    JOIN Recipes r ON r.user_id = u.id
-  //     WHERE
-  // 	    f.user_id = '${user_id}'
-  //     `);
-  //     return response[0];
-  //   }
+  public async getFeed(user_id: string): Promise<any> {
+    const response = await this.getConnection().raw(`
+    SELECT
+    p.id,
+    p.description,
+    p.createAt,
+p.creator_id,
+    p.type,
+    u.name
+  FROM Post p
+  JOIN User u ON p.creator_id = u.id
+  JOIN Friendship f ON p.creator_id = f.friend_id OR p.creator_id = f.user_id
+  WHERE (f.friend_id = '${user_id}') AND p.creator_id <> '${user_id}'
+  ORDER BY createAt DESC;
+      `);
+    return response[0];
+  }
 }
